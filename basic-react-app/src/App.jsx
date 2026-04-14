26a9f079cf06ab3164891773cd06a54ac42575bb
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2026-04-14 18:47:03 +0530

    Created reusable components and separated CSS for better structure

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,19 +1,10 @@
 import "./App.css";
 import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
 
-function Description() {
-  return <h3>I am the Description!</h3>;
-}
 
 function App() {
-  return (
-    <div>     
-      <Title/>
-      <Description/>
-      <Title/>
-      <Description/>
-    </div>
-  );
+  return <ProductTab/>;
 }
 
 export default App;
