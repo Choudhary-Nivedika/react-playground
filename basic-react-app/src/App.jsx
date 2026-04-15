a141c70403307a58ecbacf52c0f0771eb2533790
NivedikaChoudhary <nivedikachoudhary2@gmail.com>
2026-04-15 16:43:05 +0530

    Added components with styling

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Title from "./Title.jsx";
 import ProductTab from "./ProductTab.jsx";
+import MsgBox from "./MsgBox.jsx";
 
 
 function App() {
-  return <ProductTab/>;
+  return (
+    <>
+    <MsgBox userName="nivedika" textColor="red" />
+    <MsgBox userName="priya" textColor="blue" />
+    <MsgBox userName="Aaisha" textColor="green" />
+    <ProductTab />
+    </>
+  );
 }
 
 export default App;
